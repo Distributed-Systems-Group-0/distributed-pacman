dcf9bea1613df0a9c3ce8f526e462ae77ad8dfeb
Karthik Venkatesh <kvenkate@tcd.ie>
2025-03-18 18:01:07 +0000

    mongo db connection made

--- server/main.py
+++ server/main.py
@@ -1,7 +1,37 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+import os
 
-app = FastAPI()
+load_dotenv()
+MONGO_USERNAME = os.getenv('MONGO_USERNAME')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
 
+#MONGO DB CONNECTION
+# define a lifespan method for fastapi
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Start the database connection
+    await startup_db_client(app)
+    yield
+    # Close the database connection
+    await shutdown_db_client(app)
+
+# method for start the MongoDb Connection
+async def startup_db_client(app):
+    app.mongodb_client = AsyncIOMotorClient(
+        "mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@cluster0.wk2xt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    app.mongodb = app.mongodb_client.get_database("player")
+    print("MongoDB connected.")
+
+# method to close the database connection
+async def shutdown_db_client(app):
+    app.mongodb_client.close()
+    print("Database disconnected.")
+
+# creating a server with python FastAPI
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
